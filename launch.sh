9ca67fba3c35a5bce5d45fb3b1b92b96a7a36a1e
uwudwagonl <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-10 09:03:34 +0200

    I WILL KILL HITLER

---
+++ launch.sh
@@ -0,0 +1,35 @@
+task launch {
+    doLast {
+        def taskNames = gradle.startParameter.taskNames
+        def launchIndex = taskNames.indexOf('launch')
+        def passedArgs = []
+
+        if (launchIndex >= 0 && launchIndex + 1 < taskNames.size()) {
+            passedArgs = taskNames[(launchIndex + 1)..-1]
+        }
+
+        println "Args passed to launch task: $passedArgs"
+
+        def isServer = passedArgs.contains('--s') || passedArgs.contains('--server')
+
+        if (isServer) {
+            println "Launching server..."
+            if (System.properties['os.name'].toLowerCase().contains('windows')) {
+                exec {
+                    commandLine 'cmd', '/c', 'gradlew.bat', 'runServer'
+                }
+            } else {
+                exec {
+                    commandLine './gradlew', 'runServer'
+                }
+        
+         else {
+            println "Launching client..."
+            if (System.properties['os.name'].toLowerCase().contains('windows')) {
+                exec {
+                    commandLine 'cmd', '/c', 'gradlew.bat', 'runClient'
+                }
+            } else {
+                exec {
+                    commandLine './gradlew', 'runClient'
+                }
